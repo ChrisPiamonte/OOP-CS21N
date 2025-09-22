a034744c6b09b52e93a8654f744584e4c6393606
rapadagian <dcgiankyle@gmail.com>
2025-09-23 00:32:09 +0800

    Add comments for the sections of the code

--- MP1/Group02.java
+++ MP1/Group02.java
@@ -14,17 +14,20 @@
 
 public class Group02 {
   public static void main(String[] args) {
+    // Initialization of variables
     int number = 5625;
     int sumOfDigits = 0;
     int productOfDigits = 1;
     int digitDivider = 10;
 
+    // Loop for computing the sum and product of each digits
     while (number != 0) {
-      sumOfDigits += number % digitDivider;
-      productOfDigits *= number % digitDivider;
-      number /= digitDivider;
+      sumOfDigits += number % digitDivider;         // Obtain the sum of the digits of number
+      productOfDigits *= number % digitDivider;     // Obtain the product of the digits of number
+      number /= digitDivider;                       // Removes the last digit of the number
     }
 
+    // Printing of Outputs 
     System.out.println("The sum of the digits of 5256 is " + sumOfDigits);
     System.out.println("The product of the digits of 5256 is " + productOfDigits);
   }
