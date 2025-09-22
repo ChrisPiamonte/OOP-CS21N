a41d392bb92739e1584343bfbb4b2a5e8ae6b486
jayjarito07 <jayjarito07@gmail.com>
2025-09-22 14:48:42 +0800

    trial push

--- MP1/Group10.java
+++ MP1/Group10.java
@@ -15,5 +15,21 @@
 public class Group10 {
   public static void main(String[] args) {
 
+    // Initialize variables
+    int n = 10;      // Number of terms in the fibonacci sequence
+    int intFirstNumber = 0;     // First number in the sequence
+    int intSecondNumber = 1;    // Second number in the sequence
+    int intPlaceholder = 0;     // Placeholder for the next value in the sequence
+    int intSum = 0;     // Variable for the sum of all values
+        
+    // Use loop to print and add values
+    for (int i = 0; i < n; i++) {
+      System.out.print(intFirstNumber + " ");     // Print each values in the sequence
+      intPlaceholder = intFirstNumber + intSecondNumber;      // Add the initial 2 values and assign it to the placeholder 
+      intFirstNumber = intSecondNumber;       // Assign the second number to the first number
+      intSecondNumber = intPlaceholder;       // Assign the value in our placceholder to our intSecondNumber
+      intSum += intFirstNumber;       // Add all the values
+    }
+    System.out.println("\nSum of all values: " + intSum);   // Print the sum of all values
   }
 }
