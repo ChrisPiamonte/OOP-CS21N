b9c8c7faa58c27b125bf779ffb58bce292e583b6
Yasurem <jmcas88@gmail.com>
2025-09-22 14:54:50 +0800

    Trial Upload

--- MP1/Group10.java
+++ MP1/Group10.java
@@ -15,5 +15,34 @@
 public class Group10 {
   public static void main(String[] args) {
 
+    // Print the first n Fibonacci numbers 
+    int n = 10;
+
+    // Initialize the first two addends to generate the Fibonacci sequence
+      // Sequence initialization (these variables will represent the two numbers that forms the succeeding values)
+    int firstAddend = 0, secondAddend = 1;
+
+    // The second addend is the current highest value at this point in program time
+    int succeedingValue = secondAddend;
+    int summationValue = succeedingValue; // Initialize the variable for the sum of all the numbers in the sequence
+
+    // For loop was chosen since it already integrates a counter (i) which is necessary for the sequence's limit
+    for (int i = 0; i < n; i++) {
+      // Print the first two values in the Fibonacci sequence
+      System.out.print(firstAddend + ", "); // Formating purposes
+
+      // The succeeding value in the sequence can be acquired by adding the two numbers before it
+      succeedingValue = firstAddend + secondAddend;
+
+      // Update the values of the two numbers
+      firstAddend = secondAddend;
+      secondAddend = succeedingValue;
+
+      // Add the current values together
+      summationValue = summationValue + succeedingValue;
+    }
+
+    // Print the sum of all the numbers in the Fibonacci Sequence once the loop ends
+    System.out.print("\nSummation: " + summationValue + "\n\n");
   }
 }
