4c6755def0b37920fad59ac3ba58fa97b845b4ac
Nathanielle <janielpaz12345@gmail.com>
2025-09-22 19:14:50 +0800

    test

--- MP1/Group01.java
+++ MP1/Group01.java
@@ -13,7 +13,16 @@
  */
 
 public class Group01 {
-  public static void main(String[] args) {
+    public static void main(String[] args) 
+    { 
+        System.out.println("Leap years between 1900 and 2100:");
 
-  }
+        for (int year = 1900; year <= 2100; year++) 
+        {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) 
+            {
+                System.out.println(year);
+            }
+        }
+    }
 }
