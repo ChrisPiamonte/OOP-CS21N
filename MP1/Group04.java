42f744b4e2f9fff093f42d240932c465dc00b886
Psalm Francis Caadan <caadanpsalmfrancis.g@gmail.com>
2025-09-23 17:17:58 +0800

    Added grade conversion logic and variable initialization

--- MP1/Group04.java
+++ MP1/Group04.java
@@ -14,6 +14,23 @@
 
 public class Group04 {
   public static void main(String[] args) {
+    //Initialization of variables
+    int intGrade = 90; //Hardcoded grade variable
+    char charGradeLetter = ' ';
+
+    //Converting grade to letter
+    if(intGrade >= 90 && intGrade <= 100) {
+      charGradeLetter ='A';
+    } else if(intGrade >= 80 && intGrade <= 89) {
+      charGradeLetter ='B';
+    } else if(intGrade >= 70 && intGrade <= 79) {
+      charGradeLetter ='C';
+    } else if(intGrade >= 60 && intGrade <= 69) {
+      charGradeLetter ='D';
+    } else {
+      charGradeLetter = 'F';
+    }
+
 
   }
 }
