6674a91ac8d21f95eeefa03a75d509689985e76d
Chris Piamonte <chrispiamonte@Chriss-MacBook-Air.local>
2025-09-26 23:07:43 +0800

    Repository Update

--- MP1/Group06.java
+++ MP1/Group06.java
@@ -1,19 +1,30 @@
-/*
- * ==========================================================
- *  Machine Problem 1 - Workspace for Group 06
- * ==========================================================
- *
- * Instructions:
- * - This file is your workspace for solving Machine Problem 1.
- * - You will practice the Programming Core Concepts discussed in class,
- *   such as variables, data types, operators, and basic string operations.
- * - Do NOT create additional classes or advanced features (like arrays)
- *   unless instructed.
- * ==========================================================
- */
+public class Group06{
+    public static void main(String[] args) {
+        // Hardcode the limit to any number
+        int intLimit = 12;
 
-public class Group06 {
-  public static void main(String[] args) {
+        // declaration of variable for the sum of cubes
+        int intSumCube = 0;
 
-  }
+        System.out.println("Number Square Cube");
+
+        // for loop to iterate every number to n
+        for (int intNumber = 1; intNumber <= intLimit; intNumber++) {
+            
+        // formula to get the square of i
+            int intSquare = intNumber * intNumber;
+
+        // formula to get the cube of i
+            int intCube = intNumber * intNumber * intNumber;
+
+        // prints the table of square and cubes of i
+            System.out.println(intNumber + "\t" + intSquare + "\t" + intCube);
+
+        // calculates the sum of all cubes 
+            intSumCube += intCube;
+        }
+
+        // prints the sum of all cubes
+        System.out.println("\nThe sum of all cubes from 1 to " + intLimit + ": " + intSumCube);
+    }
 }
