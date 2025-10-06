3610ec230e5b5100150549806f3fa1858df867ef
Russel Bagay <russelvbagay333@gmail.com>
2025-10-06 20:34:17 +0800

    Added comments to items 1-3

--- MP2/MP2-G4/Item3.java
+++ MP2/MP2-G4/Item3.java
@@ -4,32 +4,40 @@ import java.io.IOException;
 
 public class Item3 {
     public static void main(String[] args) throws NumberFormatException, IOException{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//Reads user input
 
+        //Ask the user to input the start of the range
         System.out.print("Enter start of range: ");
         int intStart = Integer.parseInt(reader.readLine());
 
+        //Ask the user to input the end of the range
         System.out.print("Enter end of range: ");
         int intEnd = Integer.parseInt(reader.readLine());
 
-        int intTotal = 0;
+        int intTotal = 0;//To count how many prime numbers are found
 
+        //Display for output
         System.out.print("Prime numbers between " + intStart + " and " + intEnd + ": ");
 
+        //Loop through all the numbers in the given range
         for (int i = intStart; i <= intEnd; i++){
+            //To check if the current number is prime
             if (isPrime(i)){
                 System.out.print(i + " ");
                 intTotal++;
             }
         }
+        //Display the total prime numbers found
         System.out.print("\nThe total of prime number found: " + intTotal);
     }
+    //To check if a number is prime
     static boolean isPrime(int intNumber){
         if (intNumber <= 1) return false;
         for (int i = 2; i <= Math.sqrt(intNumber); i++){
             if (intNumber % i == 0)
-            return false;
+            return false;//Not a prime number if divisible by another number
         }
-        return true;
+        return true;//Prime if no divisors were found
     }
+
 }
