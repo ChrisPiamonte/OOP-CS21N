5e1efd43974c2ccff4c60f60f1bdb3a3951849fd
Russel Bagay <russelvbagay333@gmail.com>
2025-10-06 20:27:28 +0800

    Added comments to items 1-3

--- MP2/MP2-G4/Item2.java
+++ MP2/MP2-G4/Item2.java
@@ -4,21 +4,30 @@ import java.io.IOException;
 
 public class Item2 {
     public static void main(String[] args) throws NumberFormatException, IOException{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//Read user input
 
+        //Ask the user to input how many terms of the Fibonacci sequence to display
         System.out.print("Enter number of terms (n): ");
         int intFibonacci = Integer.parseInt(reader.readLine());
 
+        //Initialize the first two numbers of the Fibonacci sequence and the sum
         int intA = 0, intB = 1, intSum = 0;
 
+        //Display Fibonacci output
         System.out.print("Fibonacci Sequence: ");
+
+        //Loop to generate Fibonacci sequence
         for (int i = 0; i <= intFibonacci; i++){
-            System.out.print(intA + " ");
-            intSum += intA;
+            System.out.print(intA + " ");//Print the current Fibonacci number
+            intSum += intA;//Add current number to total sum
+
+            //Compute the next term of the sequence
             int intAdd = intA + intB;
-            intA = intB;
-            intB = intAdd;
+            intA = intB;//Second term becomes first term
+            intB = intAdd;//New term becomes second term
         }
+        //Display the sum of all the Fibonacci terms shown
         System.out.println("\nSum of sequence = " + intSum);
     }
+
 }
